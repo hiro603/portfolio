e4ee8060027eb385af84e2929b1ac85648870521
nng88 <200077567+hiro603@users.noreply.github.com>
2026-05-27 21:13:58 +0900

    Update portfolio site

---
+++ projects/site-c/assets/js/main.js
@@ -0,0 +1,14 @@
+import { initializeHamburgerMenu } from "./component/hamburger-menu.js";
+import { initializeHeaderBackgroundToggle } from "./component/header-background-toggle.js";
+import { initializeCultureGallery } from "./slider/gallery-slider.js";
+import { initializeAboutStaffSlider } from "./slider/staff-slider.js";
+import { initializeScrollFade } from "./fade/scroll-fade.js";
+// import { initializeFormValidation } from "./form/form-validation.js";
+
+// 全ての初期化を実行
+initializeHamburgerMenu();
+initializeHeaderBackgroundToggle();
+initializeCultureGallery();
+initializeAboutStaffSlider();
+initializeScrollFade();
+// initializeFormValidation();
